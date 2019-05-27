a86f654588bfec23f63a16a7d91b12c49ae4eaa4
Taranjeet Singh <reachtotj@gmail.com>
2019-05-27 22:39:55 +0530

    add argon theme and a base template.

--- src/djlibrary/urls.py
+++ src/djlibrary/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
 from django.contrib import admin
+from django.views.generic import TemplateView
 
 urlpatterns = [
 
     path('admin/', admin.site.urls),
+    path('hello', TemplateView.as_view(template_name='__base.html')),
 ]
